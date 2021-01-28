3d8296f24f350dabc1a182e5ffd76096bbbb7685
noman-khalid-dev <noman.khalid.freelance@gmail.com>
2021-01-28 13:54:21 +0500

    Create new class that hold our stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'InputPage.dart';
 
 void main() {
  runApp(BMICalculator());
@@ -9,39 +10,12 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme:ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0a0d22),//custom color
         scaffoldBackgroundColor: Color(0xFF0a0d22),//custom color
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.white),
-        )
       ),
       home: InputPage(),
     );
   }
 }
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
 
